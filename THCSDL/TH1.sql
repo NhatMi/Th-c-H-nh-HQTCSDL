7a85a3949de0f07fbb9568a52f598158c7f917ff
NhatMi <103920552+NhatMi@users.noreply.github.com>
2023-02-15 11:05:04 +0700

    Add files via upload

---
+++ THCSDL/TH1.sql
@@ -0,0 +1,115 @@
+﻿
+USE Sales
+--1.
+EXEC sp_addtype 'Mota','NVARCHAR(40)';
+EXEC sp_addtype 'IDKH','CHAR(10)','NOT NULL';
+EXEC sp_addtype 'DT', 'CHAR(12)';
+
+--2.bang
+CREATE TABLE SanPham
+(
+	Masp char(6) not null ,
+	Tensp varchar(20),
+	NgayNhap date,
+	DVT char(10),
+	SoLuongTon Int,
+	DonGiaNhap money,
+	primary key(Masp)
+  )
+
+
+  CREATE TABLE HoaDon
+(
+	MaHD char(10) not null,
+	NgayLap  date not null,
+	NgayGiao date not null,
+	MaKH IDKH not null
+	primary key(MaHD)
+  )
+   CREATE TABLE KhachHang
+   (
+   MaKH IDKH,
+   TenKH nvarchar(30) ,
+   DiaChi nvarchar(40) ,
+   DienThoai DT,
+   primary key (MaKH)
+   )
+
+   create table ChiTietHD
+   (
+		MaHD char(10) not null,
+		Masp char(6) not null,
+		SoLuong int
+		primary key (MaHD, Masp)
+   )
+--3. 
+ALTER TABLE HoaDon
+ALTER COLUMN DienGiai nvarchar(100)
+--4.
+ALTER TABLE SanPham
+ADD TyLeHoaHong float
+--5.
+ALTER TABLE SanPham
+DROP COLUMN NgayNhap
+--6.
+ALTER TABLE HoaDon
+ADD
+CONSTRAINT fk_KhachHang_HoaDon FOREIGN KEY(MaKH) REFERENCES KhachHang(MakH)
+
+ALTER TABLE ChiTietHD
+ADD
+CONSTRAINT FK_HoaDon_ChiTietHD FOREIGN KEY(MaHD) REFERENCES HoaDon(MaHD),
+CONSTRAINT FK_SanPham_ChiTietHD FOREIGN KEY(MaSp) REFERENCES SanPham(Masp)
+--7.
+------NgayGiao >= NgayLap---------
+ALTER TABLE HoaDon
+ADD CONSTRAINT check_Ngaygiao CHECK (Ngaygiao >= Ngaylap);
+------MaHD gồm 6 ký tự, 2 ký tự đầu là chữ, các ký tự còn lại là số-----
+ALTER TABLE HoaDon
+ADD CONSTRAINT check_MaHD CHECK(MaHD like '[A-Z][A-Z][0-9][0-9][0-9][0-9]');
+------Giá trị mặc định ban đầu cho cột NgayLap luôn luôn là ngày hiện hành-----
+ALTER TABLE HoaDon
+ADD CONSTRAINT df_Ngaylap DEFAULT GETDATE() FOR Ngaylap;
+
+--8.
+------SoLuongTon chỉ nhập từ 0 đến 500----
+ALTER TABLE SanPham
+ADD CONSTRAINT check_SoluongTon CHECK (SoluongTon BETWEEN 0 AND 500)
+------DonGiaNhap lớn hơn 0------
+ALTER TABLE SanPham
+ADD CONSTRAINT check_DongiaNhap CHECK (DongiaNhap > 0)
+------Giá trị mặc định cho NgayNhap là ngày hiện hành-----
+ALTER TABLE SanPham
+ADD CONSTRAINT df_NgayNhap DEFAULT GETDATE() FOR NgayNhap;
+------DVT chỉ nhập vào các giá trị ‘KG’, ‘Thùng’, ‘Hộp’, ‘Cái’----
+ALTER TABLE SanPham
+ADD CONSTRAINT df_DVT CHECK (DVT = N'KG' or DVT = N'Thùng' or DVT = N'Hộp' or DVT = N'Cái')
+--9.
+
+INSERT INTO SanPham (Masp, Tensp, NgayNhap, DVT, SoluongTon, DongiaNhap, TyLeHoaHong)
+ VALUES ('SP1', N'Bút chì', '2022-08-14', N'Cái', 200, 3000, 0.15)
+ INSERT INTO SanPham (Masp, Tensp, NgayNhap, DVT, SoluongTon, DongiaNhap, TyLeHoaHong)
+ VALUES ('SP2', N'Khẩu Trang', '2022-10-18', N'Thùng', 50, 2500, 0.10)
+ INSERT INTO SanPham (Masp, Tensp, NgayNhap, DVT, SoluongTon, DongiaNhap, TyLeHoaHong)
+ VALUES ('SP3', N'Bút Bi', '2022-10-25', N'Cái', 50, 5000, 0.15)
+
+INSERT INTO KhachHang (MaKH, TenKH, Diachi, Dienthoai)
+VALUES ('KH1', N'Minh Toàn', N'TP.HCM', '0945578442')
+INSERT INTO KhachHang (MaKH, TenKH, Diachi, Dienthoai)
+VALUES ('KH2', N'Toàn Thắng', N'Tây Ninh', '012345678')
+INSERT INTO KhachHang (MaKH, TenKH, Diachi, Dienthoai)
+VALUES ('KH3', N'Minh Thuận', N'Tây Ninh', '023145698')
+
+INSERT INTO HoaDon (MaHD, Ngaylap, Ngaygiao, MaKH, Diengiai)
+VALUES ('HD0001', '2022-08-16', '2022-08-25', 'KH1', N'Sản phẩm bút chì loại 1')
+INSERT INTO HoaDon (MaHD, Ngaylap, Ngaygiao, MaKH, Diengiai)
+VALUES ('HD0002', '2022-10-20', '2022-11-05', 'KH2', N'Sản phẩm khẩu trang kháng khuẩn')
+INSERT INTO HoaDon (MaHD, Ngaylap, Ngaygiao, MaKH, Diengiai)
+VALUES ('HD00013', '2022-08-25', '2022-08-30', 'KH3', N'Sản phẩm bút bi loại 1')
+
+INSERT INTO ChiTietHD (MaHD, Masp, Soluong)
+VALUES ('HD0001', 'SP1', 300)
+INSERT INTO ChiTietHD (MaHD, Masp, Soluong)
+VALUES ('HD0002', 'SP2', 800)
+INSERT INTO ChiTietHD (MaHD, Masp, Soluong)
+VALUES ('HD0005', 'SP3', 500)
---
+++ THCSDL/TH3_4.sql
@@ -0,0 +1,94 @@
+﻿--A.
+--B.
+CREATE TABLE Students (
+StudentID NVARCHAR(12) PRIMARY KEY,
+StudentName NVARCHAR(25) not null,
+DateofBirth Datetime not null,
+Email NVARCHAR(40),
+Phone NVARCHAR(12),
+Class NVARCHAR(10),
+)
+CREATE TABLE Subjects (
+SubjectID NVARCHAR(10) PRIMARY KEY,
+SubjectName NVARCHAR(25) not null,
+)
+CREATE TABLE Mark (
+StudentID NVARCHAR(12),
+SubjectID NVARCHAR(10),
+Date Datetime not null,
+theory Tinyint,
+Practical Tinyint,
+PRIMARY KEY (StudentID, SubjectID)
+);
+--C.
+INSERT INTO Students 
+VALUES ('AV0807005', 'Mail Trung Hiếu', '11/10/1989', 'trunghieu@yahoo.com', '0904115116', 'AV1'),
+('AV0807006', 'Nguyễn Quý Hùng', '2/12/1988', 'quyhung@yahoo.com', '0955667787', 'AV2'),
+('AV0807007', 'Đỗ Đắc Huỳnh', '2/1/1990', 'dachuynh@yahoo.com', '0988574747', 'AV2'),
+('AV0807009', 'An Đăng Khuê', '6/3/1986', 'dangkhue@yahoo.com', '0986757463', 'AV1'),
+('AV0807010', 'Nguyễn T. Tuyết Lan', '12/7/1989', 'tuyetlan@gmail.com', '0983310342', 'AV2'),
+('AV0807011', 'Đinh Phụng Long', '2/12/1990', 'phunglong@yahoo.com', NULL, 'AV1'),
+('AV0807012', 'Nguyễn Tuấn Nam', '2/3/1990', 'tuannam@yahoo.com', NULL, 'AV1');
+
+INSERT INTO Subjects 
+VALUES ('S001', 'SQL'),
+('S002', 'Java Simplefield'),
+('S003', 'Active Server Page');
+
+INSERT INTO Mark 
+VALUES ('AV0807005', 'S001','6/5/2008', 8, 25),
+('AV0807006', 'S002','6/5/2008', 16, 30),
+('AV0807007', 'S001','6/5/2008', 10, 25),
+('AV0807009', 'S003', '6/5/2008', 7, 13),
+('AV0807010', 'S003', '6/5/2008', 9, 16),
+('AV0807011', 'S002', '6/5/2008', 8, 30),
+('AV0807012', 'S001', '6/5/2008', 7, 31),
+('AV0807005', 'S002', '6/6/2008', 12, 11),
+('AV0807010', 'S001', '6/6/2008', 7, 6);
+--D.
+---1. 
+SELECT * FROM Students
+---2.
+SELECT Class, StudentID, StudentName, DateofBirth, Email, Phone
+FROM Students
+WHERE Class = 'AV1'
+---3. 
+UPDATE Students
+SET Class = 'AV2'
+WHERE StudentID = 'AV0807012'
+---4.
+SELECT Class, COUNT(*) as 'Tổng số sinh viên'
+FROM Students
+GROUP BY Class
+---5.
+SELECT *
+FROM Students
+WHERE Class = 'AV2'
+ORDER BY StudentName ASC
+---6.
+SELECT StudentName 
+FROM Students S INNER JOIN Mark M
+ON S.StudentID = M.StudentID 
+WHERE M.SubjectID = 'S001' AND  M.theory < 10 AND M.Date = '6/5/2008'
+ORDER BY StudentName ASC;
+---7.
+SELECT COUNT(*) 
+FROM Students S INNER JOIN Mark M
+ON S.StudentID = M.StudentID
+WHERE M.SubjectID = 'S001' AND  M.theory < 10 
+---8.
+SELECT * 
+FROM Students 
+WHERE Class = 'AV1' AND DateofBirth > '1/1/1980';
+---9.
+SELECT * 
+FROM Students 
+WHERE Class = 'AV1' AND DateofBirth > '1/1/1980';
+---10.
+SELECT Students.StudentID, StudentName, SubjectName, Theory, Practical, Date 
+FROM Mark 
+INNER JOIN Subjects ON Mark.SubjectID = Subjects.SubjectID 
+INNER JOIN Students ON Mark.StudentID = Students.StudentID 
+WHERE Subjects.SubjectID = 'S001' AND Date = '6/5/2008';
+
+
